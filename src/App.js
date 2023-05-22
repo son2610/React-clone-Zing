02977572834a9be43502acd4f51da3c4098744e8
Son-Blue <okmen586999@gmail.com>
2023-05-22 17:10:03 +0700

    p2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {useState} from 'react';
 
 function App() {
+  const [work, setWork] = useState('');
+  console.log(work);
   return (
-    <div className="App">
-      gp
+    <div className="flex gap-8 h-screen border justify-center items-center">
+      <input
+        type="text"
+        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+        value={work}
+        onChange={e => setWork(e.target.value)}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
     </div>
   );
 }
